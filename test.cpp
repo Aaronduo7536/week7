649aef69d7fa26aef57b00b9aab62759dbdf2c83
Aaronduo7536 <aadu7536@colorado.edu>
2024-10-13 16:36:11 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 git init
 
-echo "Hello Git!" > hello.txt
+echo "Github" > hello.txt
 git add hello.txt
 git commit -m "Initial commit"
 git remote add origin https://github.com/aadu7536/week7.git
 git push -u origin master
+
 
 
 
